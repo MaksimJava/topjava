e539f69e1b0d4841f369f2e8f9820fb2836cd13b
Maksim <vad831s56@rambler.ru>
2017-07-17 00:10:10 +0300

    HW01

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,48 @@
+package ru.javawebinar.topjava.web;
+
+/**
+ * Created by Максим on 16.07.2017.
+ */
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealWithExceed;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
+import static ru.javawebinar.topjava.util.MealsUtil.getFilteredWithExceeded;
+
+public class MealServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(MealServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LOG.debug("redirect to mealList");
+        List<Meal> mealList = Arrays.asList(
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 400),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
+        );
+
+        List<MealWithExceed> filteredMealsWithExceeded =
+                getFilteredWithExceeded(mealList, LocalTime.of(0, 0), LocalTime.of(23, 0), 2000);
+
+        req.setAttribute("mealList", filteredMealsWithExceeded);
+        RequestDispatcher Dispatcher = getServletContext().getRequestDispatcher("/mealList.jsp");
+
+        Dispatcher.forward(req, resp);
+    }
+}
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,37 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Максим
+  Date: 16.07.2017
+  Time: 23:34
+  To change this template use File | Settings | File Templates.
+--%>
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix="f" uri="http://mycompany.com" %>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
+<head>
+    <title>mealList</title>
+</head>
+<body>
+<table style=" border-style: solid; border-width:1px; width: 600px; border-collapse: collapse;">
+    <thead>
+    <tr style="background-color: gray;">
+        <td style="width: 30px;">date</td>
+        <td style="width: 80px;">description</td>
+        <td style="width: 80px;">calories</td>
+    </tr>
+    </thead>
+    <c:forEach var="meal" items="${mealList}">
+
+        <tr style="color: <c:out value="${meal.exceed ? 'red':'green'}"/>">
+            <td>${f:matches(meal.dateTime, 'dd-MM-yyyy HH:mm')}</td>
+            <td><c:out value="${meal.description}" /></td>
+            <td><c:out value="${meal.calories}" /></td>
+        </tr>
+    </c:forEach>
+</table>
+</body>
+</html>
